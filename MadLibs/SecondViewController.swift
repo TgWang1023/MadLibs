073a6a7d4277f226d701c327191901d4d6aae290
Tiange Wang <wtgkevin@gmail.com>
2018-09-10 12:01:18 -0700

    first commit

---
+++ MadLibs/SecondViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SecondViewController.swift
+//  MadLibs
+//
+//  Created by Tiange Wang on 9/9/18.
+//  Copyright © 2018 Tiange Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var adjLabel: UITextField!
+    @IBOutlet weak var verbLabel1: UITextField!
+    @IBOutlet weak var verbLabel2: UITextField!
+    @IBOutlet weak var nounLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    var word: String = "..."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mainLabel.text = word
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+        let source = segue.source as! SecondViewController
+        word = "We are having a perfectly \(source.adjLabel.text!) time now. Later we will \(source.verbLabel1.text!) and \(source.verbLabel2.text!) in the \(source.nounLabel.text!)"
     }
 
 
